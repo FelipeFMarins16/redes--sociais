a656f606bf866e047ddb3fd043a0a4eb0930d0c3
Felipe Febraio Marins <108589612+FelipeFMarins16@users.noreply.github.com>
2024-10-09 08:19:30 -0300

    Update InformaçõesGlobais.js

--- InformaçõesGlobais.js
+++ InformaçõesGlobais.js
@@ -1,2 +1,10 @@
 const url="https://raw.githubusercontent.com/felipemarins/api/main/dados-globais.json"
 
+async function visualizarInformacoesGlobais(params){
+  const res=await fetch(url)
+  const dados = res.json()
+  const paragrafo= document.createElement('p')
+  paragrafo.classlist.add('graficos-container__texto')
+  
+}
+function visualizarInformacoesGlobais()
