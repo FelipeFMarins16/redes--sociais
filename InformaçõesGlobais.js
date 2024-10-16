3de7dcc3a320eb339316e060a6b4b70fac035184
Felipe Febraio Marins <108589612+FelipeFMarins16@users.noreply.github.com>
2024-10-16 08:03:03 -0300

    Update InformaçõesGlobais.js

--- InformaçõesGlobais.js
+++ InformaçõesGlobais.js
@@ -3,8 +3,10 @@ const url="https://raw.githubusercontent.com/felipemarins/api/main/dados-globais
 async function visualizarInformacoesGlobais(params){
   const res=await fetch(url)
   const dados = res.json()
+  console.log(dados)
   const paragrafo= document.createElement('p')
   paragrafo.classlist.add('graficos-container__texto')
-  
+  paragrafo.innerHTML='voce sabia que o mundo tem ${dados.total_pessoas_mundo}de pessoas e que aproximadamente ${total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conctadas.'
+  console.log(paragrafo)
 }
 function visualizarInformacoesGlobais()
