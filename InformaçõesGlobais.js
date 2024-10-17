dbfd43e2bb7c861ed77e3b300debdddf91777bda
Felipe Febraio Marins <108589612+FelipeFMarins16@users.noreply.github.com>
2024-10-17 09:52:20 -0300

    Update InformaçõesGlobais.js

--- InformaçõesGlobais.js
+++ InformaçõesGlobais.js
@@ -6,7 +6,7 @@ async function visualizarInformacoesGlobais(params){
   console.log(dados)
   const paragrafo= document.createElement('p')
   paragrafo.classlist.add('graficos-container__texto')
-  paragrafo.innerHTML='voce sabia que o mundo tem ${dados.total_pessoas_mundo}de pessoas e que aproximadamente ${total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conctadas.'
+  paragrafo.innerHTML='voce sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span>de pessoas e que aproximadamente <span>${total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conctadas.'
   console.log(paragrafo)
   const container=document.getElementyById('graficos-container')
   container.appendChild(paragrafo)
