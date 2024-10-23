ad6f784638b08191b1e48d5774aefbb8f4b9a222
Felipe Febraio Marins <108589612+FelipeFMarins16@users.noreply.github.com>
2024-10-23 08:00:23 -0300

    Update InformaçõesGlobais.js

--- InformaçõesGlobais.js
+++ InformaçõesGlobais.js
@@ -6,10 +6,11 @@ async function visualizarInformacoesGlobais(params){
   const pessoasconctadas = (dados.total_pessoas_conectadas / 1e9)
   const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
   const horas = parseInt(dados.tempo_medio)
+  const minutos = Math.round((dados. tempo-medio - horas) * 100)
   const paragrafo= document.createElement('p')
   paragrafo.classlist.add('graficos-container__texto')
-  paragrafo.innerHTML='voce sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span>de pessoas e que aproximadamente <span>${pessoasConectadas}bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conctadas.'
-  const container=document.getElementyById('graficos-container')
+  paragrafo.innerHTML='voce sabia que o mundo tem <span> ${dados.total_pessoas_mundo}</span>de pessoas e que aproximadamente <span>${pessoasConectadas}bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas}horas </span> e <span>${minutos}minutos </span>conctadas.
+  c onst container=document.getElementyById('graficos-container')
   container.appendChild(paragrafo)
 }
 function visualizarInformacoesGlobais()
