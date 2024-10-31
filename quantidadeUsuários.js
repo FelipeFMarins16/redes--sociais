de4b43716fa2e3f6b12fa68005d2831738dc5ec3
Felipe Febraio Marins <108589612+FelipeFMarins16@users.noreply.github.com>
2024-10-31 09:26:26 -0300

    Update quantidadeUsuários.js

--- quantidadeUsuários.js
+++ quantidadeUsuários.js
@@ -1,1 +1,7 @@
 async function quantidadeUsuarios () {
+  const url="https://raw.githubusercontent.com/felipemarins/api/main/numero-usuarios.json"
+  const res = await fetch(url)
+  const dados = await res.json()
+  console.log (dados)
+}
+quantidadeUsuarios()
