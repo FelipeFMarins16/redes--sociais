d985cf68d734acb9d6604e3016acbaccec71cd71
Felipe Febraio Marins <108589612+FelipeFMarins16@users.noreply.github.com>
2024-11-01 10:43:27 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,4 +1,8 @@
 const getCSS = (variavel) => {
   return getComputedStyle(document.body).getPropertyValue(variavel)
 }
+const tickconfig ={
+  color:getCSS('--primary-color'),
+  size:16,
+  family:getCSS ('--font')
  export {getCSS}
